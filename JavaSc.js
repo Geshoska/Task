36b617aab467a04d7787d974018e3f9cd9da0d85
Geshoska <geshoskaa@gmail.com>
2022-07-28 13:01:39 +0200

    Updated files

--- JavaSc.js
+++ JavaSc.js
@@ -1,13 +1,42 @@
-
 function add(){
-    let textNode = document.getElementById('myInput');
-	let par = document.getElementById('par'); 
-    let btn = document.getElementById('new');
-    btn = document.getElementById("par").appendChild(textNode);
+
+    /*let textNode = document.getElementById('myInput');
+	let lista = document.getElementById('lista');
+    let item = document.createElement("li");
+    item.innerHTML = textNode.value;    
+    lista.appendChild(item);*/
+    
+    let x= document.getElementById('myInput');
+    let li=document.createElement('li');
+    let label=document.createElement('label');
+    let span=document.createElement('span');
+    let inp=document.createElement('checkbox');
+    
+    span.innerHTML=x.value;
+    let bullet= ul.appendChild(li);
+    bullet.appendChild(inp);
+    li.appendChild(span);
+    
 }
-function remove(){
-	let textNode = document.getElementById('myInput');
-	let par = document.getElementById('par'); 
-    let btn1 = document.getElementById('rem');
-    btn1 = document.getElementById("par").removeChild(textNode);
-} 
+function remove() {
+    let e=document.querySelector("li:last-child");
+    e.parentElement.removeChild(e);
+}
+
+
+function strike(){
+    let list = document.getElementById('ul');      
+    let checkbox = document.createElement('input');
+    checkbox.type='checkbox';
+             
+    list.appendChild(checkbox);
+
+
+    for (let i;i<x.length;i++){
+        x[i].style.textDecoration = "line-through";
+        //x[i].style.textDecoration='line-through';
+    }
+    //x.strike;
+}
+
+  
