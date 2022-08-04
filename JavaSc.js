0e316d1194900f4adbff353a9cb9d67fdee07b81
Geshoska <geshoskaa@gmail.com>
2022-08-04 10:30:06 +0200

    Final

--- JavaSc.js
+++ JavaSc.js
@@ -2,36 +2,43 @@ function add() {
   let li = document.createElement("li");
   let inp = document.getElementById("myInput").value;
   let txt = document.createTextNode(inp);
-  //let Span = document.createElement('span');
-  //Span.appendChild(txt);
-  li.appendChild(txt);
+  let Span = document.createElement('span');
+  Span.id='Span';
+  Span.appendChild(txt);
+  li.appendChild(Span);
   if (inp === '') {
     alert("You must input toDo!");
+    return;
+  }
+
+  let span = document.createElement('span');
+  let nameS = document.createTextNode('Remove');
+  span.className='remove';
+  span.addEventListener('click',rem);
+  span.appendChild(nameS);
+  li.appendChild(span);
+    
+  let checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'check';
+  checkbox.addEventListener('change',strikeItem);
+  li.appendChild(checkbox);
+ 
+
+  document.getElementById("ul").appendChild(li);
+}
+
+function rem(){
+  let remove = document.getElementsByClassName('remove');
+  let div=this.parentElement;
+  div.remove();
+}  
+
+function strikeItem() {
+  let item = this.parentElement;
+  if (this.checked) {
+    item.classList.add('check');
   } else {
-    document.getElementById("ul").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-let span = document.createElement('span');
-let nameS = document.createTextNode('Remove');
-span.className='remove';
-span.appendChild(nameS);
-li.appendChild(span);
-
-
-
-let remove = document.getElementsByClassName('remove');
-
-for(let i =0; i<remove.length;i++){
-  remove[i].onclick = function(){
-    let div=this.parentElement;
-    div.remove();
+    item.classList.remove('check');
   }
 }
-
-let checkbox = document.createElement('input');
-checkbox.type='checkbox';
-checkbox.className = 'check';
-li.appendChild(checkbox);
-}
-
