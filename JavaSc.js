91669d10caa39b82d800162d499e36b2a9e32903
Geshoska <geshoskaa@gmail.com>
2022-08-22 12:44:07 +0200

    New

--- JavaSc.js
+++ JavaSc.js
@@ -3,6 +3,7 @@ let niza = [];
 
 function add(){
   let li = document.createElement("li");
+  li.id='li';
   let inp = document.getElementById("myInput").value;
   let txt = document.createTextNode(inp);
   let Span = document.createElement('span');
@@ -33,7 +34,7 @@ function add(){
   let checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkbox.className = 'check';
-  checkbox.addEventListener('change',strikeItem);
+  checkbox.addEventListener('click',strike);
   li.appendChild(checkbox);
 
   document.getElementById("ul").appendChild(li);
@@ -43,18 +44,33 @@ function rem(){
   let li=this.parentElement;
   for (let i=0; i<niza.length;i++) {
     if(li.firstChild.innerText === niza[i].content){
-      niza.splice(i,1);
-      li.remove();     
+       li.remove();
+       niza.splice(i,1);
     }
-  }
+  } 
 }
-
-
-function strikeItem() {
-  let item = this.parentElement;
-  if (this.checked) {
-    item.classList.add('check');
-  } else {
-    item.classList.remove('check');
+function strike(event) {
+  let checkBox = [];
+  let listLi= [];
+  let checkboxes = document.getElementsByClassName('check');
+  let li = document.getElementById('li');
+    for(let i=0 ;i<niza.length;i++){
+      listLi.push(niza[i].content);
+      checkboxes[i].checked;
+      checkBox.push(checkboxes[i].checked);
+        
+      if(niza[i].content === checkBox[i].checked){
+      niza[i].done = true;
+    } 
+  } 
+ /*
+  if(checkBox[i].checked && listLi[i]){
+    //niza[i].done = true;
+    document.getElementById('li').classList.add('check');
   }
+  if(checkboxes[i].checked === false){
+    niza[i].done=false;
+    document.getElementById('li').classList.remove('check');
+  }*/
+    
 }
