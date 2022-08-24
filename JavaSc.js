9b050a2447ebbbefab50d1e02f883589aaddcdb3
Geshoska <geshoskaa@gmail.com>
2022-08-24 09:19:30 +0200

    Final

--- JavaSc.js
+++ JavaSc.js
@@ -1,66 +1,68 @@
 let niza = [];
 
-
-function add(){
+function add() {
   let li = document.createElement("li");
-  li.id='li';
+  li.id = "li";
   let inp = document.getElementById("myInput").value;
   let txt = document.createTextNode(inp);
-  let Span = document.createElement('span');
-  Span.id='Span';
+  let Span = document.createElement("span");
+  Span.id = "Span";
   Span.appendChild(txt);
   li.appendChild(Span);
-  
+
   let object = {
-    content:document.getElementById('myInput').value,
-    done:false
+    element: li,
+    content: document.getElementById("myInput").value,
+    done: false,
   };
   niza.push(object);
-  localStorage.setItem('Array', JSON.stringify(niza));
+  localStorage.setItem("Array", JSON.stringify(niza));
   console.log(niza);
 
-  if (inp === '') {
+  if (inp === "") {
     alert("You must input toDo!");
-    return;   
+    return;
   }
 
-  let span = document.createElement('span');
-  let nameS = document.createTextNode('Remove');
-  span.className='remove';
-  span.addEventListener('click',rem);
+  let span = document.createElement("span");
+  let nameS = document.createTextNode("Remove");
+  span.className = "remove";
+  span.addEventListener("click", rem);
   span.appendChild(nameS);
   li.appendChild(span);
-    
-  let checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.className = 'check';
-  checkbox.addEventListener('click',strike);
+
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "check";
+  checkbox.addEventListener("click", strike);
   li.appendChild(checkbox);
 
   document.getElementById("ul").appendChild(li);
 }
 
-function rem(){
-  let li=this.parentElement;
-  for (let i=0; i<niza.length;i++) {
-    if(li.firstChild.innerText === niza[i].content){
-       li.remove();
-       niza.splice(i,1);
+function rem() {
+  let li = this.parentElement;
+  for (let i = 0; i < niza.length; i++) {
+    if (li.firstChild.innerText === niza[i].content) {
+      li.remove();
+      niza.splice(i, 1);
     }
-  } 
+  }
 }
 
-function strike (){
-  let item = this.parentElement;
-  let checkbox = document.getElementsByClassName('check');
-  for (let i=0;i<niza.length;i++){
-  if(checkbox[i].checked && niza[i].content){
-    item.classList.add('check');
-    niza[i].done=true;
+function strike() {
+  let checkbox = this;
+  let item = checkbox.parentElement;
+  for (let i = 0; i < niza.length; i++) {
+    let ref = niza.find((el) => el.element === item);
+    if (ref !== undefined) {
+      if (checkbox.checked && niza[i].content) {
+        item.classList.add("check");
+        ref.done = true;
+      } else {
+        item.classList.remove("check");
+        ref.done = false;
+      }
+    }
   }
-  else{
-  item.classList.remove('check'); 
-  niza[i].done=false;
-  }
-  }  
 }
